0f3b77b18e19a7dfebd822f04b2119ad1fe4d3c3
Tim Savage <tim@savage.company>
2018-04-12 01:28:15 +1000

    Added imports and helper fields

---
+++ odin/__init__.py
@@ -0,0 +1,11 @@
+""""
+Odin
+""""
+__authors__ = "Tim Savage"
+__author_email__ = "tim@savage.company"
+__copyright__ = "Copyright (C) 2018 Tim Savage"
+__version__ = "3.0"
+
+from .fields import *  # noqa
+# from .fields.composite import *  # noqa
+# from .fields.virtual import *  # noqa
