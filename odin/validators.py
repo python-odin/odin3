c73ca3583c8bd69946e06874fd3774f5b0b86970
Tim Savage <tim@savage.company>
2018-04-15 22:16:21 +1000

    Ported email, and IP validators

--- odin/validators.py
+++ odin/validators.py
@@ -2,12 +2,14 @@
 Odin validators
 
 """
+import abc
 import re
 
-from typing import Pattern, Union, Any, Sized
+from typing import Pattern, Union, Any, Sized, Callable, TypeVar
 
 from . import exceptions
 from .typing import Number
+from .utils.ipv6 import is_valid_ipv6_address
 
 EMPTY_VALUES = (None, '', [], (), {})
 
@@ -58,7 +60,7 @@ class URLValidator(RegexValidator):
 validate_url = URLValidator()
 
 
-class BaseValidator:
+class BaseValidator(metaclass=abc.ABCMeta):
     """
     Common base for validators.
     """
@@ -81,8 +83,11 @@ class BaseValidator:
     def __str__(self) -> str:
         return self.description.format(limit_value=self.limit_value)
 
+    @abc.abstractmethod
     def compare(self, a: Any, b: Any) -> bool:
-        raise NotImplementedError()
+        """
+        Comparison function.
+        """
 
     def clean(self, value: Any) -> Any:
         """
@@ -119,9 +124,9 @@ class LengthValidator(BaseValidator):
     """
     Validate a value exactly the expected length.
     """
-    message = 'Ensure this value has exactly %(limit_value)d characters (it has %(show_value)d).'
+    message = 'Ensure this value has exactly {limit_value:d} characters (it has {show_value:d}).'
     code = 'length'
-    description = 'Ensure value has exactly %(limit_value)d characters.'
+    description = 'Ensure value has exactly {limit_value:d} characters.'
 
     def compare(self, a: Any, b: Any) -> bool:
         return a != b
@@ -134,9 +139,9 @@ class MaxLengthValidator(LengthValidator):
     """
     Validate a values length is less than the limit value.
     """
-    message = 'Ensure this value has at most %(limit_value)d characters (it has %(show_value)d).'
+    message = 'Ensure this value has at most {limit_value:d} characters (it has {show_value:d}).'
     code = 'max_length'
-    description = 'Ensure value has at most %(limit_value)d characters.'
+    description = 'Ensure value has at most {limit_value:d} characters.'
 
     def compare(self, a: Any, b: Any) -> bool:
         return a > b
@@ -146,9 +151,143 @@ class MinLengthValidator(LengthValidator):
     """
     Validate a values length is greater than the limit value.
     """
-    message = 'Ensure this value has at least %(limit_value)d characters (it has %(show_value)d).'
+    message = 'Ensure this value has at least {limit_value:d} characters (it has {show_value:d}).'
     code = 'min_length'
-    description = 'Ensure value has at least %(limit_value)d characters.'
+    description = 'Ensure value has at least {limit_value:d} characters.'
 
     def compare(self, a: Any, b: Any) -> bool:
         return a < b
+
+
+T = TypeVar('T')
+
+
+def simple_validator(message: str='The supplied value is invalid', code: str='invalid',
+                     assertion: Callable[[T], bool]=None) -> Callable[[T], None]:
+    """
+    Create a simple validator.
+
+    :param message: Message to raised in Validation exception if validation fails.
+    :param code: Code to included in Validation exception. This can be used to customise the message at the resource
+        level.
+    :param assertion: An Validation exception will be raised if this check returns a none True value.
+
+    Usage::
+
+        >>> none_validator = simple_validator(lambda x: x is not None, message="This value cannot be none")
+
+    This can also be used as a decorator::
+
+        >>> @simple_validator("This value cannot be none")
+        ... def none_validator(v):
+        ...     return v is not None
+
+    """
+    if callable(message):
+        message, code, assertion = 'The supplied value is invalid', 'invalid', message
+
+    def inner(func):
+        def wrapper(value):
+            params = {'show_value': value}
+            if not func(value):
+                raise exceptions.ValidationError(message.format(params), code=code, params=params)
+        return wrapper
+
+    return inner(assertion) if assertion else inner
+
+
+class IPv4Address(RegexValidator):
+    """
+    Validate an IPv4 address
+    """
+    regex = r'^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])){3}\Z'
+    message = 'Enter a valid IPv4 address.'
+
+
+validate_ipv4_address = IPv4Address()
+
+
+@simple_validator("Enter a valid IPv6 address")
+def validate_ipv6_address(value):
+    """
+    Validate an IPv6 address
+    """
+    return is_valid_ipv6_address(value)
+
+
+def validate_ipv46_address(value):
+    """
+    Validate is either an IPv4 or IPv6 address
+    """
+    try:
+        validate_ipv4_address(value)
+    except exceptions.ValidationError:
+        try:
+            validate_ipv6_address(value)
+        except exceptions.ValidationError:
+            raise exceptions.ValidationError('Enter a valid IPv4 or IPv6 address.', code='invalid')
+
+
+class EmailValidator(object):
+    """
+    Validate an Email address.
+    """
+    message = 'Enter a valid email address.'
+    code = 'invalid'
+    user_regex = re.compile(
+        r"(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*\Z"  # dot-atom
+        r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-\011\013\014\016-\177])*"\Z)',  # quoted-string
+        re.IGNORECASE)
+    domain_regex = re.compile(
+        # max length for domain name labels is 63 characters per RFC 1034
+        r'((?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+)(?:[A-Z0-9-]{2,63}(?<!-))\Z',
+        re.IGNORECASE)
+    literal_regex = re.compile(
+        # literal form, ipv4 or ipv6 address (SMTP 4.1.3)
+        r'\[([A-f0-9:.]+)\]\Z',
+        re.IGNORECASE)
+    domain_whitelist = ['localhost']
+
+    def __init__(self, message=None, code=None, whitelist=None):
+        if message is not None:
+            self.message = message
+        if code is not None:
+            self.code = code
+        if whitelist is not None:
+            self.domain_whitelist = whitelist
+
+    def __call__(self, value):
+        if not value or '@' not in value:
+            raise exceptions.ValidationError(self.message, code=self.code)
+
+        user_part, domain_part = value.rsplit('@', 1)
+
+        if not self.user_regex.match(user_part):
+            raise exceptions.ValidationError(self.message, code=self.code)
+
+        if (domain_part not in self.domain_whitelist and
+                not self.validate_domain_part(domain_part)):
+            # Try for possible IDN domain-part
+            try:
+                domain_part = domain_part.encode('idna').decode('ascii')
+                if self.validate_domain_part(domain_part):
+                    return
+            except UnicodeError:
+                pass
+            raise exceptions.ValidationError(self.message, code=self.code)
+
+    def validate_domain_part(self, domain_part):
+        if self.domain_regex.match(domain_part):
+            return True
+
+        literal_match = self.literal_regex.match(domain_part)
+        if literal_match:
+            ip_address = literal_match.group(1)
+            try:
+                validate_ipv46_address(ip_address)
+                return True
+            except exceptions.ValidationError:
+                pass
+
+
+validate_email_address = EmailValidator()
---
+++ tests/test_validators.py
@@ -0,0 +1,227 @@
+import pytest
+
+from odin import validators
+from odin.exceptions import ValidationError
+
+
+class TestValidator(object):
+    def test_regex_validator(self):
+        target = validators.RegexValidator(r'^[a-z]{3}$', "Please enter 3 alpha characters.", "match_chars")
+
+        assert "Please enter 3 alpha characters." == target.message
+        assert "match_chars" == target.code
+
+    @pytest.mark.parametrize('value', (
+        "abc",
+        "cba",
+    ))
+    def test_regex_validator__valid(self, value):
+        validators.RegexValidator(r'^[a-z]{3}$')(value)
+
+    @pytest.mark.parametrize('value', (
+        "a",
+        "abcd",
+        "123",
+    ))
+    def test_regex_validator__invalid(self, value):
+        with pytest.raises(ValidationError):
+            validators.RegexValidator(r'^[a-z]{3}$')(value)
+
+    @pytest.mark.parametrize('value', (
+        'http://www.djangoproject.com/',
+        'http://localhost/',
+        'http://example.com/',
+        'http://www.example.com/',
+        'http://www.example.com:8000/test',
+        'http://valid-with-hyphens.com/',
+        'http://subdomain.example.com/',
+        'http://200.8.9.10/',
+        'http://200.8.9.10:8000/test',
+        'http://valid-----hyphens.com/',
+        'http://example.com?something=value',
+        'http://example.com/index.php?something=value&another=value2',
+        'https://example.com/',
+        'ftp://example.com/',
+        'ftps://example.com/',
+        'http://savage.company/',
+    ))
+    def test_validate_url__valid(self, value):
+        validators.validate_url(value)
+
+    @pytest.mark.parametrize('value', (
+        'foo',
+        'http://',
+        'http://example',
+        'http://example.',
+        'http://.com',
+        'http://invalid-.com',
+        'http://-invalid.com',
+        'http://inv-.alid-.com',
+        'http://inv-.-alid.com',
+    ))
+    def test_validate_url__invalid(self, value):
+        with pytest.raises(ValidationError):
+            validators.validate_url(value)
+
+    def test_max_value_validator(self):
+        target = validators.MaxValueValidator(10)
+
+        target(10)
+        target(1)
+        target(-10)
+        pytest.raises(ValidationError, target, 11)
+
+    def test_min_value_validator(self):
+        target = validators.MinValueValidator(10)
+
+        target(10)
+        pytest.raises(ValidationError, target, 1)
+        pytest.raises(ValidationError, target, -10)
+        target(11)
+
+    def test_length_validator(self):
+        target = validators.LengthValidator(10)
+        target("1234567890")
+        pytest.raises(ValidationError, target, "123456789")
+        pytest.raises(ValidationError, target, "12345678901")
+
+    def test_max_length_validator(self):
+        target = validators.MaxLengthValidator(10)
+
+        target("123457890")
+        target("12345")
+        target("")
+        pytest.raises(ValidationError, target, "12345678901")
+
+    def test_min_length_validator(self):
+        target = validators.MinLengthValidator(10)
+
+        pytest.raises(ValidationError, target, "123457890")
+        pytest.raises(ValidationError, target, "12345")
+        pytest.raises(ValidationError, target, "")
+        target("12345678901")
+
+
+class TestSimpleValidator(object):
+    def test_method(self):
+        def reflect(v):
+            return v
+
+        validator = validators.simple_validator(assertion=reflect)
+
+        validator(True)
+        pytest.raises(ValidationError, validator, False)
+
+    def test_decorator(self):
+        @validators.simple_validator
+        def reflect_validator(v):
+            return v
+
+        reflect_validator(True)
+        pytest.raises(ValidationError, reflect_validator, False)
+
+
+@pytest.mark.parametrize('value', (
+    '192.168.0.1',
+    '1.1.1.1',
+    '255.255.255.255',
+))
+def test_ipv4_address_valid(value):
+    validators.validate_ipv4_address(value)
+
+
+@pytest.mark.parametrize('value', (
+    '.2.3.4',
+    '1.2.3',
+    '1.2.3.4.5',
+    '300.0.0.1',
+    'ff.ff.ff.ff',
+    'abcd',
+    '',
+    '::ff:ff:ff:ff'
+))
+def test_ipv4_address_invalid(value):
+    with pytest.raises(ValidationError):
+        validators.validate_ipv4_address(value)
+
+
+@pytest.mark.parametrize('value', (
+    '::',  # Localhost
+    '::1',  # Default unicast
+    '::ffff:0:0',  # IPv4 Mapped address
+    '2002::',  # 6to4
+    '2001::',  # Teredo tunneling
+    'fc00::',  # Unique local address
+    'ffff::192.168.0.1',
+    '1:2:3:4:5:6:7:8',
+    'dead:beef:dead:beef:dead:beef:dead:beef'
+))
+def test_ipv6_address_valid(value):
+    validators.validate_ipv6_address(value)
+
+
+@pytest.mark.parametrize('value', (
+    '192.168.0.1',
+    '1:2:3:4:5:6:7:8:9',
+    '1234:5678:9abc:defg::',
+    '2001::12345',
+    '::ffff::',
+    'ff:::0',
+    ':2:3:4:5:6:7:8',
+    '1:2:3:4:5:6:7:',
+    ':2:3:4:5:6:7:',
+    '1:2:3:4:5:192.168.0',
+    '1:2:3:4:256.256.256.256',
+    '192.168.0.1::ff',
+    'zzzz::0',
+))
+def test_ipv6_address_invalid(value):
+    with pytest.raises(ValidationError):
+        validators.validate_ipv6_address(value)
+
+
+@pytest.mark.parametrize('value', (
+    '192.168.0.1',
+    '1:2:3:4:5:6:7:8',
+))
+def test_validate_ipv46_address_valid(value):
+    validators.validate_ipv46_address(value)
+
+
+@pytest.mark.parametrize('value', (
+    '192.168.0.',
+    '1:2:3:4:5:6:7:8:9',
+))
+def test_validate_ipv46_address_invalid(value):
+    with pytest.raises(ValidationError):
+        validators.validate_ipv46_address(value)
+
+
+@pytest.mark.parametrize('value kwargs'.split(), (
+    ('foo@bar.com', None),
+    ('FOO@BAR.COM', None),
+    ('foo@localhost', None),
+    # New release domain names
+    ('foo@example.company', None),
+    # IPv4 based email
+    ('foo@[127.0.0.1]', None),
+    # IPv6 based email
+    ('foo@[::1]', None),
+))
+def test_validate_email_valid(value, kwargs):
+    kwargs = kwargs or {}
+    validators.EmailValidator(**kwargs)(value)
+
+
+@pytest.mark.parametrize('value kwargs'.split(), (
+    ('foo', None),
+    ('.foo@bar.com', None),
+    ('foo@corpau', None),
+    ('foo@localhost', {'whitelist': ''}),
+    ('foo@{0}.com'.format('a' * 64), None),
+    ('foo@[::ff::0]', None),
+))
+def test_validate_email_invalid(value, kwargs):
+    kwargs = kwargs or {}
+    with pytest.raises(ValidationError):
+        validators.EmailValidator(**kwargs)(value)
